1dbd8c2642808578ecdf0d6ba2e24903e2969089
BSEF19A520 <bsef19a520@pucit.edu.pk>
2022-06-18 15:51:34 +0500

    added backend to the app

--- app/src/main/java/com/example/changetext/MainActivity.java
+++ app/src/main/java/com/example/changetext/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.changetext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView text1;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text1 = (TextView) findViewById(R.id.text);
     }
 }
